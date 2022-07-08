9a0f746932543090ae3cf0cc5368b296c51f5e32
Anas Khan <akhan2@ischoolconnect.com>
2022-07-08 18:56:04 +0530

    header component is created

--- src/app/school-explore/school-explore.module.ts
+++ src/app/school-explore/school-explore.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SchoolExploreRoutingModule } from './school-explore-routing.module';
+import { HeaderComponent } from './screens/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     SchoolExploreRoutingModule
